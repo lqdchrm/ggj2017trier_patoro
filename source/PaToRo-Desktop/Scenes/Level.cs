d3e95af8bdc3cbc63aece8a6fa9c9059d2fcd87e
Robert Rößger <robert.roessger@globalfacilities.lu>
2017-01-20 21:36:12 +0100

    added level

---
+++ source/PaToRo-Desktop/Scenes/Level.cs
@@ -0,0 +1,66 @@
+﻿using PaToRo_Desktop.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace PaToRo_Desktop.Scenes
+{
+    public class Level : Entity
+    {
+        private readonly BaseGame game;
+        public int NumValues {  get { return upper.Length; } }
+
+        private Texture2D part;
+
+        public float[] upper;
+        public float[] lower;
+
+        public Level(BaseGame game, int num)
+        {
+            this.game = game;
+
+            upper = new float[num];
+            lower = new float[num];
+
+            FillStatic();
+        }
+
+        private void FillStatic()
+        {
+            for (int i = 0; i < upper.Length; ++i)
+            {
+                upper[i] = (float)(game.Screen.Height * 0.5f) - 50;
+                lower[i] = (float)(game.Screen.Height * 0.5f) + 50;
+            }
+        }
+
+        internal void LoadContent(ContentManager content)
+        {
+            part = content.Load<Texture2D>("Images/particle");
+        }
+
+        internal override void Update(GameTime gameTime)
+        {
+
+        }
+
+        internal override void Draw(SpriteBatch spriteBatch, GameTime gameTime)
+        {
+            Vector2 pos = Vector2.Zero;
+
+            for (int i = 0; i < NumValues; ++i)
+            {
+                pos.X = i * (game.Screen.Width / NumValues);
+                pos.Y = upper[i];
+                spriteBatch.Draw(part, pos, Color.White);
+                pos.Y = lower[i];
+                spriteBatch.Draw(part, pos, Color.White);
+            }
+        }
+    }
+}
---
+++ source/PaToRo-Desktop/Scenes/LevelScene.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.Xna.Framework;
+using PaToRo_Desktop.Engine;
+using PaToRo_Desktop.Engine.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaToRo_Desktop.Scenes
+{
+    public class LevelScene : Scene
+    {
+        private DebugOverlay dbgOverlay;
+        private Ball ball;
+        private Level level;
+
+        public LevelScene(BaseGame game) : base(game)
+        {
+        }
+
+        internal override void Initialize()
+        {
+            base.Initialize();
+            BgColor = Color.Black;
+        }
+
+        internal override void LoadContent()
+        {
+            base.LoadContent();
+
+            level = new Level(game, 64);
+            level.LoadContent(game.Content);
+            Children.Add(level);
+
+            ball = new Ball();
+            ball.LoadContent(game.Content);
+            ball.Phy.Pos.X = game.Screen.Width * 0.5f;
+            ball.Phy.Pos.Y = game.Screen.Height * 0.5f;
+            Children.Add(ball);
+
+            dbgOverlay = new DebugOverlay(game);
+            Children.Add(dbgOverlay);
+        }
+
+        internal override int HandleInput(GameTime gameTime)
+        {
+            var numPlayers = base.HandleInput(gameTime);
+
+            // register Players
+            if (numPlayers < 2)
+            {
+                dbgOverlay.Text = string.Format("Player {0}, please press a button", numPlayers);
+                game.Inputs.AssignToPlayer(numPlayers);
+
+            }
+
+            // Move Ball
+            if (numPlayers > 0)
+            {
+                float speed = 0;
+                speed += game.Inputs.Player(0).IsDown(Buttons.DPad_Left) ? -100f : 0f;
+                speed += game.Inputs.Player(0).IsDown(Buttons.DPad_Right) ? 100f : 0f;
+                speed += game.Inputs.Player(0).Value(Sliders.LeftStickX) * 100;
+                ball.Phy.Spd.X = speed;
+            }
+
+            if (numPlayers > 1)
+            {
+                float speed = 0;
+                speed += game.Inputs.Player(1).IsDown(Buttons.DPad_Down) ? 100f : 0f;
+                speed += game.Inputs.Player(1).IsDown(Buttons.DPad_Up) ? -100f : 0f;
+                speed += game.Inputs.Player(1).Value(Sliders.LeftStickY) * 100;
+
+                ball.Phy.Spd.Y = speed;
+            }
+
+            // Rumble
+            if (numPlayers > 1)
+            {
+                dbgOverlay.Text = "Use Triggers to rumble";
+
+
+                if (game.Inputs.Player(0).Value(Sliders.LeftTrigger) > 0.2
+                 || game.Inputs.Player(0).Value(Sliders.RightTrigger) > 0.2)
+                {
+                    game.Inputs.Player(1).Rumble(
+                        game.Inputs.Player(0).Value(Sliders.LeftTrigger),
+                        game.Inputs.Player(0).Value(Sliders.RightTrigger),
+                        200
+                    );
+                }
+
+                if (game.Inputs.Player(1).Value(Sliders.LeftTrigger) > 0.2
+                 || game.Inputs.Player(1).Value(Sliders.RightTrigger) > 0.2)
+                {
+                    game.Inputs.Player(0).Rumble(
+                        game.Inputs.Player(1).Value(Sliders.LeftTrigger),
+                        game.Inputs.Player(1).Value(Sliders.RightTrigger),
+                        200
+                    );
+                }
+            }
+
+            return numPlayers;
+        }
+    }
+}
